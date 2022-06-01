3bf7734b55e0c652ca340f19acf34a73277e7fa1
Arjun Saili <arjun7.saili@gmail.com>
2022-06-01 10:05:19 -0400

    Set up basic messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hello World",
+    user: "Arjun Saili",
+    added: new Date()
+  },
+  {
+    text: "This is a message",
+    user: "Some Person",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
 module.exports = router;
